8281d8eb17e670ae26eece4cddc637df7914f429
Jeong Myeonggu <audrn6689@gmail.com>
2023-09-18 00:49:26 +0900

    제출 이후 작업 - 테스트 코드

--- src/test/kotlin/com/s2w/asm/application/processor/SoftwareFindProcessorTest.kt
+++ src/test/kotlin/com/s2w/asm/application/processor/SoftwareFindProcessorTest.kt
@@ -1,5 +1,78 @@
 package com.s2w.asm.application.processor
 
-import org.junit.jupiter.api.Assertions.*
+import com.s2w.asm.application.AsmValidator
+import com.s2w.asm.application.dto.result.SoftwareResult
+import com.s2w.asm.common.response.PagedResponse
+import com.s2w.asm.domain.Asm
+import com.s2w.asm.domain.AsmQueryRepository
+import com.s2w.asm.domain.exception.SeedIdNotFoundException
+import io.mockk.every
+import io.mockk.justRun
+import io.mockk.mockk
+import org.assertj.core.api.Assertions.assertThat
+import io.mockk.verify
+import org.junit.jupiter.api.Assertions.assertThrows
+import org.junit.jupiter.api.Test
 
-class SoftwareFindProcessorTest
+class SoftwareFindProcessorTest {
+    private val asmQueryRepository: AsmQueryRepository = mockk()
+    private val asmValidator: AsmValidator = mockk()
+
+    private val sut = SoftwareFindProcessor(asmQueryRepository, asmValidator)
+
+    @Test
+    fun `seedId에 따라 소프트웨어를 조회할 수 있다 - 성공`() {
+        // Given
+        val seedId = "SEED_8664d1e419"
+        val page = 1
+        val size = 2
+
+        val asm1 = Asm.create("cafe.naver.com", "active", "3.38.222.16", listOf("Nginx", "Tomcat"), seedId)
+        asm1.setTestTime()
+        val asm2 = Asm.create("nid.naver.com", "active", "52.78.116.214", listOf("React", "Java"), seedId)
+        asm2.setTestTime()
+        val asm3 = Asm.create("shopping.naver.com", "active", "43.201.3.5", listOf("PWA", "styled-components", "Vue.js"), seedId)
+        asm3.setTestTime()
+
+        val asmList = listOf(asm1, asm2, asm3)
+
+        val softwareResults = asmList.map { SoftwareResult.from(it) }
+        val pagedAsmResponse = PagedResponse(asmList, asmList.size, size, page)
+        val expectedPagedSoftwareResult = PagedResponse(softwareResults, softwareResults.size, size, page)
+
+        every { asmQueryRepository.findAllBySeedId(seedId, page, size) } returns pagedAsmResponse
+        justRun { asmValidator.validateAsmList(any(), any()) }
+
+        // When
+        val result = sut.execute(seedId, page, size)
+
+        // Then
+        verify(exactly = 1) { asmQueryRepository.findAllBySeedId(seedId, page, size) }
+        verify(exactly = 1) { asmValidator.validateAsmList(pagedAsmResponse, seedId) }
+
+        assertThat(result.contents).isEqualTo(expectedPagedSoftwareResult.contents)
+        assertThat(result.totalElements).isEqualTo(expectedPagedSoftwareResult.totalElements)
+        assertThat(result.size).isEqualTo(expectedPagedSoftwareResult.size)
+        assertThat(result.currentPage).isEqualTo(expectedPagedSoftwareResult.currentPage)
+    }
+
+    @Test
+    fun `seedId에 따른 소프트웨어가 없을 경우 SeedIdNotFoundException을 발생시킨다 - 실패`() {
+        // Given
+        val seedId = "SEED_8664d1e419"
+        val page = 1
+        val size = 2
+
+        val emptyPagedAsmResponse = PagedResponse(emptyList<Asm>(), 0, size, page)
+
+        every { asmQueryRepository.findAllBySeedId(seedId, page, size) } returns emptyPagedAsmResponse
+        every { asmValidator.validateAsmList(emptyPagedAsmResponse, seedId) } throws SeedIdNotFoundException()
+
+        // When & Then
+        assertThrows(SeedIdNotFoundException::class.java) {
+            sut.execute(seedId, page, size)
+        }
+        verify(exactly = 1) { asmQueryRepository.findAllBySeedId(seedId, page, size) }
+        verify(exactly = 1) { asmValidator.validateAsmList(emptyPagedAsmResponse, seedId) }
+    }
+}
